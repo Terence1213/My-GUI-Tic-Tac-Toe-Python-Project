a6b35785275d8dff19078bfdd228e8a86413e28b
TezzaLuvsPigeons <terenceportelli2019@gmail.com>
2023-03-07 07:18:53 +0100

    Update
    > Completed main functionality of the tic tac toe game.
    > Now I have to create the AI, and menu for the game.

--- main.py
+++ main.py
@@ -1,54 +1,107 @@
 from tkinter import *
 import random
+
 window = Tk()
 
-table = [{"A1" : "-", "A2" : "-", "A3" : "-"},
-         {"B1" : "-", "B2" : "-", "B3" : "-"},
-         {"C1" : "-", "C2" : "-", "C3" : "-"},]
 
+def playTurn(row, column):
 
+    if buttons[row][column]['text'] == "" and checkWinner() == False:
 
-def restartGame():
-    pass
+        buttons[row][column].config(text=player_turn.get())
 
-def checkWinner():
-    pass
+        if checkWinner() == False:
 
-#CURRENTLY DOING: MAKING IT SO WHEN THE USER PLAYS, THE PLAY IS DISPLAYED ON THE BUTTON.
-def playTurn(player_choice):
-    global table
-    for row in table:
-        if row.get(player_choice) == "-":
-            row[player_choice] = player_turn.get()
             if player_turn.get() == "x":
                 player_turn.set("o")
+
             else:
                 player_turn.set("x")
-    player.config(text=player_turn.get())
+            player_turn_label.config(text=f"{player_turn.get()}'s turn")
+
+        elif checkWinner() == True:
+            player_turn_label.config(text=f"{player_turn.get()} wins!")
+
+        elif checkWinner() == "tie":
+            player_turn_label.config(text="Tie!")
+
+    """
+    ------------------------------------------------------------------------------------------------------------
+    AI FUNCTION OF THE TIC TAC TOE GAME - I plan to make a menu when starting the program, and then a new window
+    pops up when the user chooses the play
+    ------------------------------------------------------------------------------------------------------------ 
+    if AIEnabled and player_turn.get() = "o":
+        if buttons[row][column]['text'] == "" and checkWinner() == False:
+        CURRENTLY WORKING ON HERE - IF THE SELECTED BOX IS ALREADY OCCUPIED, THIS PART OF THE PROGRAM LOOPS
+            row = random.range(0, 3)
+            column = random.range(0, 3)
+            while buttons[row][column]['text'] != "":
+                row = random.range(0, 3)
+                column = random.range(0, 3)
+            buttons[row][column].config(text=player_turn.get())
+    """
+
+def checkAllEmpty():
+    result = 9
+    for row in range(3):
+        for column in range(3):
+            if buttons[row][column]['text'] == "":
+                result -= 1
+    print(result)
+    if result == 9:
+        return True
+    return False
+
+
+def checkWinner():
+
+    for row in range(3):
+        if buttons[row][0]['text'] == buttons[row][1]['text'] == buttons[row][2]['text'] != "":
+            return True
+
+    for column in range(3):
+        if buttons[0][column]['text'] == buttons[1][column]['text'] == buttons[2][column]['text'] != "":
+            return True
+
+    if buttons[0][0]['text'] == buttons[1][1]['text'] == buttons[2][2]['text'] != "":
+        return True
+
+    if buttons[2][0]['text'] == buttons[1][1]['text'] == buttons[0][2]['text'] != "":
+        return True
+
+    if checkAllEmpty() == True:
+        return "tie"
+
+    return False
 
 
 def startGame():
     players = ["x", "o"]
     player_turn.set(random.choice(players))
-    player.config(text=player_turn.get())
+    player_turn_label.config(text=f"{player_turn.get()}'s turn")
+    for row in range(3):
+        for column in range(3):
+            buttons[row][column]['text'] = ""
 
 player_turn = StringVar()
+player_turn_label = Label(window, text="Player turn", font=("Arial", 30))
+player_turn_label.pack()
 
 game_table = Frame(window)
-A1_button = Button(game_table, font=("Arial", 50), height=1, width=3, command=lambda: playTurn("A1")).grid(row=0, column=0)
-A2_button = Button(game_table, font=("Arial", 50), height=1, width=3, command=lambda: playTurn("A2")).grid(row=0, column=1)
-A3_button = Button(game_table, font=("Arial", 50), height=1, width=3, command=lambda: playTurn("A3")).grid(row=0, column=2)
-B1_button = Button(game_table, font=("Arial", 50), height=1, width=3, command=lambda: playTurn("B1")).grid(row=1, column=0)
-B2_button = Button(game_table, font=("Arial", 50), height=1, width=3, command=lambda: playTurn("B2")).grid(row=1, column=1)
-B3_button = Button(game_table, font=("Arial", 50), height=1, width=3, command=lambda: playTurn("B3")).grid(row=1, column=2)
-C1_button = Button(game_table, font=("Arial", 50), height=1, width=3, command=lambda: playTurn("C1")).grid(row=2, column=0)
-C2_button = Button(game_table, font=("Arial", 50), height=1, width=3, command=lambda: playTurn("C2")).grid(row=2, column=1)
-C3_button = Button(game_table, font=("Arial", 50), height=1, width=3, command=lambda: playTurn("C3")).grid(row=2, column=2)
-player = Label(window, text="Player turn", font=("Arial", 30))
-restart_button = Button(window, text="Restart", command=restartGame)
-player.pack()
+
+buttons = [[Button(game_table), Button(game_table), Button(game_table)],
+           [Button(game_table), Button(game_table), Button(game_table)],
+           [Button(game_table), Button(game_table), Button(game_table)]]
+
+for row in range(3):
+    for column in range(3):
+        buttons[row][column] = Button(game_table, font=("Arial", 50), height=1, width=3,
+                                      command=lambda row=row, column=column: playTurn(row, column))
+        buttons[row][column].grid(row=row, column=column)
+
+restart_button = Button(window, text="Restart", font=("Arial", 15), command=startGame)
+restart_button.pack()
 game_table.pack()
 
 startGame()
 window.mainloop()
-
